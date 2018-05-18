cb97f66f87f38027a72e2708658b406fc868d63a
Melanie Downing <sayanything830@gmail.com>
2018-05-18 08:51:32 -0700

    still debugging heroku deployment

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 
 app.use(express.static(`${__dirname}/build`));
 
+app.get('/',(request,response) => {
+  response.sendFile(`${__dirname}/build/index.html`);
+});
+
 app.listen(process.env.PORT,() => {
   console.log('__SERVER_UP__',process.env.PORT);
 });
