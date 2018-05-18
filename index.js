190f95ce805c4a18c6a708553bfb0a136ece0c17
Melanie Downing <sayanything830@gmail.com>
2018-05-18 09:17:26 -0700

    heroku debug continued

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/',(request,response) => {
   response.sendFile(`${__dirname}/build/index.html`);
 });
 
+app.get('*',(request,response) => {
+  response.sendFile(`${__dirname}/build/index.html`);
+});
+
 app.listen(process.env.PORT,() => {
   console.log('__SERVER_UP__',process.env.PORT);
 });
